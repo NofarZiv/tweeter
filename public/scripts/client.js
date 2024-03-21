52fdb487f681e98d7ba3ce075132f7f665d161dc
NofarZiv <nofarziv13@gmail.com>
2024-03-21 12:43:54 -0230

    added ajax get request for tweets and added time stamps.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,30 +9,30 @@ $(document).ready(function () {
   console.log("Document is ready!");
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
 
 const createTweetElement = function(tweet) {
@@ -53,7 +53,7 @@ const createTweetElement = function(tweet) {
     </header>
 
     <footer>
-      <p>${tweet.created_at}</p>
+      <p>${new Date(tweet.created_at)}</p>
      <div class="icons">
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
@@ -74,7 +74,6 @@ const renderTweets = function(tweets) {
 }
 
 
-renderTweets(data);
 
 $(".formTweet").submit(function(event) {
   event.preventDefault();
@@ -85,7 +84,17 @@ $(".formTweet").submit(function(event) {
       console.log('Success: ', data);
       
     });
-
 });
+
+
+const loadTweets = function() {
+
+    $.ajax({ url: '/tweets', method: 'GET', dataType: "json" })
+    .then(function (data) {
+      renderTweets(data);
+    });
+  };
+
+loadTweets();
 
 });
